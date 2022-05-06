0d6eeb626ef1bf8f948716e22b65725af271bf44
mrose5 <manamayakibarose@gmail.com>
2022-05-06 14:33:49 -0400

    working search

--- keywords.js
+++ keywords.js
@@ -6,13 +6,14 @@ loadResourceAsync('/API/aapi-schema', {returntype: 'json'})
       return ((a.toLowerCase() > b.toLowerCase())?1:-1);
     });
 
-    let indexLett = '!';
     let alphabet = String.fromCharCode(...Array(91).keys()).slice(65);
     let phantomHTML = ''; 
     for (let character of alphabet) {
-      phantomHTML += '<button class="nav-buttons" id= "gloss_button_' + character + '" disabled>' + character + '</button>';
+      phantomHTML += '<input type = "radio" class="nav-buttons" id= "gloss_button_' + character + '" name="nav-bar" disabled>' 
+      + '<label for="gloss_button_' + character + '" id="bttn_label_' + character + '" disabled>' + character + '</label>' 
+      + '<div class="label-content" id="keywords_' + character + '"></div>';
     }
-    document.getElementById('nav_bar_restapi').innerHTML = phantomHTML;
+    document.getElementById('nav_bar_restapi').innerHTML += phantomHTML;
     phantomHTML = '';
 
 
@@ -20,20 +21,6 @@ loadResourceAsync('/API/aapi-schema', {returntype: 'json'})
       document.documentElement.scrollTo({top: 0, behavior: 'smooth'});
       console.log("scroll");
     }, false);
-
-    function showHideCard(indexLett) {
-      keys.forEach(keyword => {
-        showCard = document.getElementById(keyword);
-
-        if(keyword.charAt(0).toUpperCase() == indexLett) {
-          showCard.style.display = 'block';
-        } else {
-          showCard.style.display = 'hidden';
-        }
-        });
-      console.log(document.getElementById(keyword));
-    }
-
 
     keys.forEach(keyword => {
       
@@ -47,8 +34,8 @@ loadResourceAsync('/API/aapi-schema', {returntype: 'json'})
       
       const phantomSyntax = schema[keyword]["syntax"]?'<li><b>Syntax:</b> <code>' + schema[keyword]["syntax"] + '</code> </li>':'';      
 
-      phantomHTML += 
-        '<div class="restapi-keyword-container keyword-' + keyword[0].toLowerCase() + '" id="' + keyword + '">'
+      phantomHTML = 
+        '<div class="restapi-keyword-container keyword-' + keyword[0].toUpperCase() + '" id="' + keyword + '">'
          +   '<h2 class="restapi-keyword-styling">' + keyword + '</h2>'
          +   '<dl>' 
          +     '<dt>' + schema[keyword]["title"] + '</dt>' 
@@ -63,26 +50,47 @@ loadResourceAsync('/API/aapi-schema', {returntype: 'json'})
          +     '</dd>' 
          +     '</dl>' 
          + '</div>';
-      
-
-      let showCard;
-      if (indexLett != keyword.charAt(0).toUpperCase()) {
-         indexLett = keyword.charAt(0).toUpperCase();
-         let bttnIndexLett = document.getElementById('gloss_button_' + indexLett);
-         bttnIndexLett.disabled = false;
-         bttnIndexLett.addEventListener('click', function(evt) {
-           let cards = document.getElementsByClassName("restapi-keyword-container");
-           for (var i = 0; i < cards.length; i++) {
-             showCard = cards.item(i);
-             if(showCard.id.charAt(0).toUpperCase() == evt.target.id.slice(-1)) {
-               showCard.style.display = 'block';
-             } else {
-               showCard.style.display = 'none';
-             }
-           }
-        }, false);
-      }
+      document.getElementById("keywords_" + keyword[0].toUpperCase()).innerHTML += phantomHTML;
+      document.getElementById("keywords_search").innerHTML += phantomHTML;
+      document.getElementById("gloss_button_" + keyword[0].toUpperCase()).disabled = false;
+      document.getElementById("bttn_label_" + keyword[0].toUpperCase()).disabled = false;
     });
-  document.getElementById('keyword_card_container').innerHTML = phantomHTML;
+   document.getElementById("gloss_button_A").checked = true;
 });
+
+let buttonMemory = '';
+document.getElementById("restapi-search").addEventListener("search", function(event) {
+    document.getElementById(buttonMemory).checked = true;
+    console.log(buttonMemory);
+});
+
+function Search() {
+  let radioFound = document.querySelector('input[name="nav-bar"]:checked');
+  let input = document.getElementById("restapi-search").value.toLowerCase();
+  let containerArray = document.getElementById("keywords_search").children;
+  let length = document.getElementById("restapi-search").value.length;
+  console.log(containerArray);
+  console.log(input);
+
+  if(radioFound.id != "button_search") {
+    buttonMemory = radioFound.id;
+  }
+  
+  console.log(buttonMemory);
+  
+  for(i = 0; i < containerArray.length; i++) {
+    if(containerArray[i].innerHTML.toLowerCase().includes(input)) {
+      containerArray[i].style.display = "block";
+    } else {
+      containerArray[i].style.display = "none";
+    }
+  }
+  
+  if(length > 2) {
+    document.getElementById("button_search").checked = true;
+  } else {
+    document.getElementById(buttonMemory).checked = true;
+  }
+}
+
 buildHeaderFooter('..');
